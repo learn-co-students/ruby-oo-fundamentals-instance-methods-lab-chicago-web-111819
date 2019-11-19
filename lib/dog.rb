54efe1ecec36b06b1690008a9c9a97a88b374a2d
hfarnsworth <havilahfarnsworth@gmail.com>
2019-11-19 11:51:02 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+class Person
+    def talk
+        puts "Hello World!"
+    end
+end
